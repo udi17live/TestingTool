a222e380306ff6eb5ac56749deb62d1e9538a8aa
Uditha Melan <uditha@vetstoria.com>
2019-08-22 14:16:44 +0530

    Input file names of input file and output file as arguments to run the script

--- main.py
+++ main.py
@@ -1,30 +1,47 @@
+import sys
+
 import yaml
 import itertools
 import csv
 
 count = 0
 
+
 while count == 0:
-    print('Enter a Input file Name:')
-    inputFileName = input()
+    # get User input from console
+    # print('Enter a Input file Name:')
+    # inputFileName = input()
+    # inputFileName = inputFileName + ".yml"
+    # print('Enter a Output file Name:')
+    # outputFileName = input()
+    # outputFileName = outputFileName + ".csv"
+
+    # get input from arguments passed
+    inputFileName = sys.argv[1]
+    outputFileName = sys.argv[2]
+
+    # adding file extensions as the input file can only be yml and output be csv
     inputFileName = inputFileName + ".yml"
-    print('Enter a Output file Name:')
-    outputFileName = input()
     outputFileName = outputFileName + ".csv"
 
     print("Your Input File Name is: ", inputFileName)
     print("Your Output File Name is: ", outputFileName)
 
+
+
     try:
+        # getting data from input file to pyyaml format for parsing
         with open(inputFileName, 'r') as stream:
             conditions = yaml.safe_load(stream)
         count = 1
     except:
         print("File Name ", inputFileName,
-              " deos not exist. Please create such file or check the extention and try again.")
+              " deos not exist. Please create such file or enter the file name properly and try again.")
         count = 0
+        sys.exit()
 
 
+# diving the all input conditions to separate lists
 parameters = conditions['parameters']
 invalid = list(conditions['invalid'])
 
@@ -44,7 +61,7 @@ iterationsOfParameters = len(paramNames) - 1
 a = 0
 b = 1
 
-# structuring the Parameters
+# structuring the Parameters for generating conditions.
 for a in range(len(firstParameter)):
     allCombinations.append([])
     allCombinations[a].append([firstParameter[a]])
@@ -55,6 +72,7 @@ invalidCombinations = []
 
 c = 0
 d = 1
+# structuring invalid conditions to opt out of the final output
 for c in range(len(invalid)):
     invalidCombinations.append([])
     invalidCombinations[c].append([invalid[c][paramNames[0]]])
@@ -70,12 +88,13 @@ list = []
 e = 0
 f = 1
 
-
+# function to compare two lists and remove the similar ones
 def diff(first, second):
     second = set(second)
     return [item for item in first if item not in second]
 
 
+# setting up final conditions based on all conditions and invalid conditions
 for e in range(len(allCombinations)):
     finalList.append([])
 
@@ -93,10 +112,12 @@ for e in range(len(allCombinations)):
 output = []
 i = 0
 k = 0
+# writing all possible conditions to a csv file.
 with open(outputFileName, 'w', newline='\n') as myfile:
     wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
     wr.writerow(paramNames)
     for i in finalList:
+        # making all possible conditions
         output = ["".join(str(x)) for x in itertools.product(*i)]
         for k in range(len(output)):
             outputStr = output[k]
